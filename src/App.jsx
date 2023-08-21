8e8806cc4b72f45ec15db65a4ac5d2e56d205ef9
direwolf292 <chandaspatelci@gmail.com>
2023-08-21 12:01:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+//import { useState } from 'react';
+//import SpaceImage from '/Space.png';
+import About from "./components/About";
+
+
+export default function App() {
+  return (
+    <div className='h-screen w-full bg-gradient-to-r from-orange-800 to-red-950'>
+      <div className='grid grid-cols-2 h-screen w-full'>
+
+        {/* Left side of the screen */}
+          <div className='bg-red-900/50 flex justify-end items-center pr-5'>
+            <div className='flex flex-col justify-end items-end'>
+              <div>
+                <p className='text-3xl text-white '>ABOUT</p>
+              </div>
+              <div>
+                <p className='text-3xl text-white'>SKILLS</p>
+              </div>
+              <div>
+                <p className='text-3xl text-white'>PROJECTS</p>
+              </div>
+              <div>
+                <p className='text-3xl text-white'>CONTACT</p>
+              </div>
+              <div>
+                <p className='text-3xl text-white'>EDUCATION AND CERTIFICTION</p>
+              </div>
+              <div>
+                <p className='text-3xl text-white'>EXPERIENCE</p>
+              </div>
+            </div>
+          </div>
+
+        {/* Right side of the screen */}
+          <div className='bg-blue-900/50 flex justify-start items-center pl-5'>
+            <div className='flex flex-col-reverse'>
+              <div>
+                <p className='text-xl'><About /></p>
+              </div>
+              <div>
+                <p className='text-xl'>Second</p>
+              </div>
+              <div>
+                <p className='text-xl'>Third</p>
+              </div>
+              <div>
+                <p className='text-xl'>Fourth</p>
+              </div>
+              <div>
+                <p className='text-xl'>Fifth</p>
+              </div>
+              <div>
+                <p className='text-xl'>Sixth</p>
+              </div>
+            </div>
+            </div>
+
+      </div>
+    </div>
+
+  );
+}
+
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+
+
+const About = () => {
+    return(
+        <div>
+            <p className="text-xl">ABC</p>
+        </div>
+    )
+}
+
+export default About
